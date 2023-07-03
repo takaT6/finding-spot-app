ee0c051f6116ccfa89d4057061eeda2ed6639c13
takagaki.taichi <takagaki.taichi@raccoon.ne.jp>
2023-07-03 18:13:18 +0900

    🐛

--- src/app/map/Card.tsx
+++ src/app/map/Card.tsx
@@ -64,9 +64,9 @@ export function Card(props: Spot) {
             </div>
           </details>
         </div>
-        <div className="">
+        <div className="text-center">
           <a href="#" className="w-3/4 inline-flex items-center px-3 py-2 text-[0.5rem] font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">
-            <div><div>場所を</div><div>確認する</div></div>
+            <div>場所を確認する</div>
             <div>→</div>
           </a>
         </div>
